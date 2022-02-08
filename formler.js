2a303513744ec70992500fd8b8ac6a11a10025d5
DinoshanY <dinoshan0710@gmail.com>
2022-02-08 11:57:36 +0100

    Update

--- formler.js
+++ formler.js
@@ -1,7 +1,38 @@
+let fodselsdato = document.getElementById("fodselsdato").value;
+let isKvinne = document.getElementById("isKvinne").value;
+let wowRace = document.getElementById("wowRace").value;
+let antallGangerRettetGeirSinProg = document.getElementById("antallGangerRettetGeirSinProg".value);
+let treningPerUke = document.getElementById("treningPerUke").value;
+let McTjukkasPerManed = document.getElementById("McTjukkasPerManed").value;
+let hasFrokostForSkole = document.getElementById("hasFrokostForSkole".value);
+let hasDrivingFemalePartner = document.getElementById("hasDrivingFemalePartner".value);
+let hasMaleDrivingPartner = document.getElementById("hasMaleDrivingPartner").value;
+let isSpillerItimen = document.getElementById("isSpillerItimen").value;
+
+
+
+
+
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log(fodselsdato.value)
+  let today = new Date();
+  let timeLeft = today.getDate()+" "+today.getUTCDay()+" "+today.getFullYear();
+
+  calculateExactTimeOfDeath(timeLeft, isKvinne, wowRace, antallGangerRettetGeirSinProg,
+    treningPerUke, McTjukkasPerManed,  
+    hasFrokostForSkole, hasDrivingFemalePartner, hasMaleDrivingPartner,
+    isSpillerItimen);
+});
+
 
 /**
  * @param {String} fodselsdato format: ddmmyy
- * @param {boolean} isKvinne //how you identify, hvis du er ikke binær, bruk det tallet som du synes beskriver kjønnet ditt best
+ * @param {number} isKvinne //how you identify, hvis du er ikke binær, bruk det tallet som du synes beskriver kjønnet ditt best
  * @param {String} wowRace //other MMO's count too.. 
  * @param {number} antallGangerRettetGeirSinProg 
  * @param {number} treningPerUke //bare kul trening teller.
@@ -18,7 +49,7 @@ function calculateAge(
     hasFrokostForSkole, hasDrivingFemalePartner, hasMaleDrivingPartner,
     isSpillerItimen){
         let calculatedAge;
-        if(isKvinne){
+        if(isKvinne = 1){
             calculatedAge = 81.7;
         } else {
             calculatedAge = 76.1;
@@ -38,10 +69,12 @@ function calculateAge(
         return calculatedAge;
     }
 
+   
+
     //Complete the formulae
     //You are supposed to calculate time and date of death, down to the minute!
     function calculateExactTimeOfDeath(
-        fodselsdato, isKvinne, wowRace, antallGangerRettetGeirSinProg,
+        timeLeft, isKvinne, wowRace, antallGangerRettetGeirSinProg,
         treningPerUke, McTjukkasPerManed,  
         hasFrokostForSkole, hasDrivingFemalePartner, hasMaleDrivingPartner,
         isSpillerItimen){
@@ -49,6 +82,11 @@ function calculateAge(
             treningPerUke, McTjukkasPerManed,  
             hasFrokostForSkole, hasDrivingFemalePartner, hasMaleDrivingPartner,
             isSpillerItimen);
+
+           let left = achievedAge - timeLeft;
+        console.log(achievedAge);
+           console.log(timeLeft)
+           console.log(left);
 
 
 
